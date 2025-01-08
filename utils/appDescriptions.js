be7740a13a458e07557418f6bef1149778049253
Geo Joy <me.geojoy@gmail.com>
2025-01-08 23:13:08 +0530

    new tools

--- utils/appDescriptions.js
+++ utils/appDescriptions.js
@@ -1,4 +1,5 @@
 export const appDescriptions = {
   TwoPointerShoppingCart:
     "Master the two-pointer technique with our interactive shopping cart tutorial. Learn how to optimize algorithms for efficient item selection and cost calculation.",
+  SipSwpCalculator: "A financial planning tool that helps you visualize your long-term investment journey. Calculate how your investments grow over time and plan your withdrawals, with detailed yearly and monthly breakdowns.",
 };
